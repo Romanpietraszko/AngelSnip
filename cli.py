612b6e9e54ba9c5a6b627679c4d13e1188d9097e
Romanpietraszko <roman.pietraszko@onet.eu>
2025-09-28 12:25:51 +0200

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,9 +1,11 @@
 import argparse
 from genprototypów import generuj_klase
 
+# === CLI ===
 parser = argparse.ArgumentParser(description='Generator prototypów')
-parser.add_argument('--nazwa', required=True)
-parser.add_argument('--komponenty', nargs='+')
+parser.add_argument('--nazwa', required=False)
+parser.add_argument('--komponenty', nargs='+', default=[])
+parser.add_argument('--prefix', required=False)
+parser.add_argument('--folder_snippetow', required=False, default="snipety")
 args = parser.parse_args()
-
 print(generuj_klase(args.nazwa, args.komponenty))
